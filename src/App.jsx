7df91a878f0082e4f15185938788242437f3bb56
mdriyadtalukder <riyadtalukder67@gmail.com>
2025-09-28 19:07:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Clients from "./sections/Clients";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import Experience from "./sections/Experience";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Projects></Projects>
+      <Clients></Clients>
+      <Experience></Experience>
+      <Contact></Contact>
+      <Footer></Footer>
+    </main>
+  );
+};
+
+export default App;
